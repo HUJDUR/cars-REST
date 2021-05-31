283943b433a85357dd12232fedb9ce36e9304eef
HUJDUR <nadir.hujdur2002@gmail.com>
2021-05-31 17:41:07 +0200

    Database Connection

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const app = require('./app.js');
 
 const port = process.env.PORT || 3000;
 
+const DB = process.env.DATABASE.replace(
+	'<PASSWORD>',
+	process.env.DATABASE_PASSWORD
+);
+
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log('Database connected!'));
+
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
 });
