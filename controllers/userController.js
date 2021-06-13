ee130e1c8183829c6565816dcdcb2b1b666187f1
HUJDUR <nadir.hujdur2002@gmail.com>
2021-06-13 15:11:56 +0200

    User Implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,10 @@
 const User = require('../models/userModel');
 const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
+const handlerFactory = require('./handlerFactory');
+
+exports.getAllUsers = handlerFactory.getAll(User);
+exports.getUser = handlerFactory.getOne(User);
+exports.createUser = handlerFactory.createOne(User);
+exports.updateUser = handlerFactory.updateOne(User);
+exports.deleteUser = handlerFactory.deleteOne(User);
